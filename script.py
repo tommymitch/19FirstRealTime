e1399e2a630cfd5e9c685796ee2dfd3895147ca7
teamaxe <teamaxe@hotmail.com>
2025-08-28 17:11:56 +0100

    Added real-time classification

--- script.py
+++ script.py
@@ -2,6 +2,37 @@ import ximu3
 import numpy as np
 from collections import deque
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+# ===============================
+# 1D CNN Model Definition
+# ===============================
+class Simple1DCNN1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=15, padding=7),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+            nn.Conv1d(32, 64, kernel_size=7, padding=3),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 1209, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 6),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
 
 class CircularBuffer:
     def __init__(self, size):
@@ -19,7 +50,7 @@ class CircularBuffer:
     def __getitem__(self, index):
         return self.buffer[index]
 
-class Counter:
+class CounterTrigger:
    def __init__(self, counter=0, trigger=False):
        self.counter = counter
        self.trigger = trigger
@@ -53,13 +84,12 @@ THRESHOLD = 2
 ATTACK = 1612
 DECAY = 3225
 
-ct = Counter()
-
+ct = CounterTrigger()
 arr = np.array([])
-
 attack_buffer = CircularBuffer(ATTACK)
 decay_buffer = CircularBuffer(DECAY)
 
+surfaces = ["soft", "medium", "hard", "airtap", "tapup", "air"]
 
 def inertial_callback(message):
     accelerometer = np.array([message.accelerometer_x, message.accelerometer_y, message.accelerometer_z])
@@ -69,21 +99,32 @@ def inertial_callback(message):
 
     if not ct.trigger and magnitude > THRESHOLD:
         ct.trigger = True
-        print(f"Triggered: {magnitude}")
 
     if not ct.trigger:
         attack_buffer.append(magnitude)
 
-    if ct.trigger and ct.counter < DECAY:
+    if ct.trigger:
         decay_buffer.append(magnitude)
         ct.increment()
         if ct.counter == DECAY:
-            print(f"Complete: {magnitude}")
             arr = np.concatenate([attack_buffer, decay_buffer])
-            # ct.reset()
+            #====
+            # Classify
+            #====
+            x_inference = torch.FloatTensor(arr).unsqueeze(0)
+            x_inference = x_inference.unsqueeze(1)
+            with torch.no_grad():  # Disable gradient computation for efficiency
+                predictions = model(x_inference)
+            predicted_class = torch.argmax(predictions, dim=1).item()
+            print(surfaces[predicted_class])
+            ct.reset()
             
     
     # print(f"Bang: {magnitude}")
+
+model = Simple1DCNN1()
+model = torch.load('model.pth', weights_only=False)
+model.eval()
  
 device = ximu3.PortScanner.scan()[0]
 connection_info = device.connection_info
